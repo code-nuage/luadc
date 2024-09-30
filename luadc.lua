c204c4616a63bc85ac3190eab12c86b456ddb5d6
Eloi Nuage Hariot <98582064+code-nuage@users.noreply.github.com>
2024-09-30 14:13:34 +0200

    Text support + bug fixing

--- luadc.lua
+++ luadc.lua
@@ -1,11 +1,14 @@
 --[[
 
+Author: Eloi Nuage Hariot
 
-
+MIT LICENSE
+Copyright (c) 2024 Eloi Nuage Hariot
 
 ]]
 
 local curses = require('curses')
+require('math')
 os.setlocale("C.UTF-8")
 
 luadc = {}
@@ -17,10 +20,12 @@ luadc = {}
 -- Values
 
 local tiles = {"█", "▓", "▒", "░"} -- Tiles with grease to draw
+local map = {} -- Map that stores blocks added with luadc.add_block()
 
 -- Interfaces
 
 local draw = function(mode, submode, x, y, sx, sy)
+    local sx, sy = sx or 1, sy or 1
     if mode == "tiles" then
         for col = 1, sy do
             for row = 1, sx do
@@ -36,28 +41,41 @@ local draw = function(mode, submode, x, y, sx, sy)
     end
 end
 
-local map = {}
-
 -- =====================
 -- = Public interfaces =
 -- =====================
 
--- Curses rebind
+-- Values
 
-function luadc.load()
+luadc.debug = false
+
+-- Init and close luadc
+
+function luadc.init()
     screen = curses.initscr() -- Setting up a screen to interact with
+    
+    luadc.WINDOW_H, luadc.WINDOW_W = screen:getmaxyx()
 
     curses.cbreak()
     curses.echo(false)
     curses.nl(false)
     curses.curs_set(0)
+
+    while true do
+
+        luadc.draw()
+        screen:refresh()
+
+        input = screen:getch()
+        
+        luadc.update()
+        screen:clear()
+    end
 end
 
-function luadc.clear()
-    screen:clear()
-end
+-- Curses rebind
 
-function luadc.input(input, listener)
+function luadc.input(listener, input)
     if input < 256 then input = string.char(input) end -- Translate the 256 first ASCII character to single string character
 
     if input == listener then -- Listen to an input to put in condition
@@ -65,11 +83,12 @@ function luadc.input(input, listener)
     end
 end
 
-function luadc.draw(mode, submode, x, y, sx, sy)
-    draw(mode, submode, x, y, sx, sy)
+-- Map management
+
+function luadc.draw_block(mode, submode, x, y, sx, sy)
+    draw(mode, submode, x - 1, y - 1, sx, sy)
 end
 
--- Collisions management
 function luadc.add_block(x, y)
     if map[y] == nil then
         map[y] = {}
@@ -78,12 +97,14 @@ function luadc.add_block(x, y)
 end
 
 function luadc.remove_block(x, y)
-    table.remove(map[y - 1], x - 1)
+    table.remove(map[y], x - 1)
 end
 
 function luadc.check(x, y)
-    if map[y][x] ~= 1 then
-        return true
+    if x >= 1 and y >= 1 and x <= luadc.WINDOW_W and y <= luadc.WINDOW_H then
+        if map[y] == nil or map[y][x] ~= 1 then
+            return true
+        end
     end
 end
 
@@ -95,9 +116,23 @@ function luadc.draw_collisions()
     end
 end
 
--- Text management
-function luadc.text()
+-- Menu management
 
+function luadc.menu(table)
+    local max_size = 0
+
+    for k, v in ipairs(table) do
+        if max_size < string.len(table[k]) then
+            max_size = string.len(table[k])
+        end
+    end
+
+    local x = math.floor(luadc.WINDOW_W / 2) - math.floor(max_size / 2)
+    local y = math.floor(luadc.WINDOW_H / 2) - math.floor(#table * 2 - 1)
+
+    for k, v in ipairs(table) do
+        screen:mvaddstr(y + k * 2 - 1, x, table[k])
+    end
 end
 
 return luadc
